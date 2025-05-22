148ea1d704d3749a1be690aaa904909940bbaca8
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-22 16:26:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let btn=document.querySelector("#enterBtn");
+
+btn.addEventListener("click",()=>{
+	let para=document.querySelector("#status");
+	para.innerHTML="<h1>Entered Metaverse</h1>";
+})
